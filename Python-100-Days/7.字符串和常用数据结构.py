b055404901420d1d827d24fda69fb5f8886617be
zero617 <604613671@qq.com>
2022-05-16 13:12:30 +0800

    7

--- Python-100-Days/7.字符串和常用数据结构.py
+++ Python-100-Days/7.字符串和常用数据结构.py
@@ -1,4 +1,5 @@
 """练习1：在屏幕上显示跑马灯文字。"""
+from random import randrange, randint, sample
 import random
 import os
 import time
@@ -96,6 +97,91 @@ def main():
     month = int(input("请输入月份："))
     day = int(input("请输入日期："))
     print(which_day(year, month, day))
+
+
+if __name__ == '__main__':
+    main()
+
+
+"""练习6：打印杨辉三角。"""
+
+
+def yh_triangle(num):
+    yh = [[]] * num
+    for row in range(len(yh)):
+        yh[row] = [None] * (row + 1)
+        for col in range(len(yh[row])):
+            if col == 0 or col == row:
+                yh[row][col] = 1
+            else:
+                yh[row][col] = yh[row - 1][col] + yh[row - 1][col - 1]
+            print(yh[row][col], end='\t')
+        print()
+
+
+if __name__ == '__main__':
+    num = int(input("请输入行数："))
+    yh_triangle(num)
+
+
+"""综合案例"""
+
+"""案例1：双色球选号。"""
+
+
+def display(balls):
+    """
+    输出列表中的双色球号码
+    """
+    for index, ball in enumerate(balls):
+        if index == len(balls) - 1:
+            print('|', end=' ')
+        print('%02d' % ball, end=' ')
+    print()
+
+
+def random_select():
+    """
+    随机选择一组号码
+    """
+    red_balls = [x for x in range(1, 34)]
+    selected_balls = []
+    selected_balls = sample(red_balls, 6)
+    selected_balls.sort()
+    selected_balls.append(randint(1, 16))
+    return selected_balls
+
+
+def main():
+    n = int(input('机选几注: '))
+    for _ in range(n):
+        display(random_select())
+
+
+if __name__ == '__main__':
+    main()
+
+
+"""
+《幸运的基督徒》
+有15个基督徒和15个非基督徒在海上遇险，为了能让一部分人活下来不得不将其中15个人扔到海里面去，有个人想了个办法就是大家围成一个圈，由某个人开始从1报数，报到9的人就扔到海里面，他后面的人接着从1开始报数，报到9的人继续扔到海里面，直到扔掉15个人。由于上帝的保佑，15个基督徒都幸免于难，问这些人最开始是怎么站的，哪些位置是基督徒哪些位置是非基督徒。
+"""
+
+
+def main():
+    persons = [True] * 30
+    counter, index, number = 0, 0, 0
+    while counter < 15:
+        if persons[index]:
+            number += 1
+            if number == 9:
+                persons[index] = False
+                counter += 1
+                number = 0
+        index += 1
+        index %= 30  # 过30归0 30循环
+    for person in persons:
+        print('基' if person else '非', end='')
 
 
 if __name__ == '__main__':
